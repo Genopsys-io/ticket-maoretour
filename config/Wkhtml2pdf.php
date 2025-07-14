fc463734e0388807d74e091fd3fc4b12613d247b
Nadjmou BOINA <boina@genopsys.io>
2025-07-15 00:59:15 +0400

    Update Wkhtml2pdf configuration for debugging and binary path settings

--- config/Wkhtml2pdf.php
+++ config/Wkhtml2pdf.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'debug'       => env('APP_DEBUG_PDF', false),
-    'binpath'     => 'lib/',
-    'binfile'     => env('WKHTML2PDF_BIN_FILE', 'wkhtmltopdf-amd64'),
+    'debug'       => env('APP_DEBUG_PDF', true),
+    'binpath'     => env('WKHTML2PDF_BIN_PATH', '/usr/bin/'),
+    'binfile'     => env('WKHTML2PDF_BIN_FILE', 'wkhtmltopdf'),
     'output_mode' => 'I',
 ];
